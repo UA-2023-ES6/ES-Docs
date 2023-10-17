a28a08ca11a3d58fe6340235de2068c03d24420a
pompeucosta <pompeugcosta@gmail.com>
2023-10-17 12:31:58 +0100

    fix deployment config issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://ua-2023-es6.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ES-Docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
